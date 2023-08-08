7c8f03126ec1e7730a54a83cb5f3a68decf807a1
Lucas Silva <97544334+lucas-dos-santos-silva@users.noreply.github.com>
2023-08-08 20:30:51 -0300

    versao 1.1

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -6,22 +6,19 @@ public class JavaApplication1 {
 
    
     public static void main(String[] args) {
-        System.out.println("digite um numero");
-        Scanner Scan = new Scanner(System.in);
-        int Num = Scan.nextInt();
-        
-        
-        
-        if(Num < 0 ){
-            System.out.println("Negativo");
-        }else if (Num > 0){
-            System.out.println("Positivo");
-        }else{
-            System.out.println("Zero");
-        }
-     
- 
+      int Valor = 8;
+      
+      if(Valor % 3 == 0 && Valor % 5 == 0){
+          System.out.println("  Multiplo de 3 e de 5");
+      }else if(Valor % 5 == 0){
+          System.out.println("apenas multiplo de 5");
+    }else if(Valor %3 == 0){
+          System.out.println("apenas multiplo de 3");
+          
+    }else{
+          System.out.println("nao multiplo de nenhum");
     }
+}
 }
     
 
