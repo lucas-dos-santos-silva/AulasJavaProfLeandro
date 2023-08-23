1d445b9afd2af2e90feb8896043c6e8c0aa33739
lucas <lucas414santos@gmail.com>
2023-08-23 09:50:08 -0300

    exercicio mmc

--- src/javaapplication5/exercicio12.java
+++ src/javaapplication5/exercicio12.java
@@ -3,7 +3,29 @@ package javaapplication5;
 
 public class exercicio12 {
     public static void main(String[] args) {
-      int ValorInteiro = 15;
-      
+        System.out.println(calculoMmc(2,3)); 
+        
+       
+    }
+    public static int calculoMmc(int a,int b){
+        int c = 2;
+        int r = 1; 
+            while(a+b!=2){
+             if(a%c==0 || b%c==0){
+            r = r * c; 
+            
+            if(a%c==0 ){
+                a = a/c;
+            }
+            if(b%c==0 ){
+                b = b/c; 
+            }
+            }
+            else{
+            c++;
+            }
+        }
+
+        return r;
     }
 }
